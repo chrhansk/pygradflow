7a863a8e9a52333c6884bf95fac80307ac68b0e7
Christoph Hansknecht <christoph.hansknecht@tu-clausthal.de>
2024-08-28 15:20:40 +0200

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.abspath(".."))
 project = "pygradflow"
 copyright = "2023, Christoph Hansknecht"
 author = "Christoph Hansknecht"
-release = "0.5.19"
+release = "0.5.20"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
